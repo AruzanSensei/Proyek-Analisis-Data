55ae82d69ada3fdfa2b0893682c9332fd80fd3cc
Aruzan Sensei <alzanadytia.j@gmail.com>
2024-08-24 15:20:57 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -1,8 +1,9 @@
 import pandas as pd
-import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
+import matplotlib.pyplot as plt
 from babel.numbers import format_currency
+
 sns.set(style='dark')
 
 st.set_option('deprecation.showPyplotGlobalUse', False)
