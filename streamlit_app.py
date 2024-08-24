0e06b1d418764b04c3f271dac32eef3f56aa71c0
Aruzan Sensei <alzanadytia.j@gmail.com>
2024-08-24 13:37:19 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,13 @@
 import streamlit
+st.text('Fixed width text')
+st.markdown('_Markdown_') # see #*
+st.caption('Balloons. Hundreds of them...')
+st.latex(r''' e^{i\pi} + 1 = 0 ''')
+st.write('Most objects') # df, err, func, keras!
+st.write(['st', 'is <', 3]) # see *
+st.title('My title')
+st.header('My header')
+st.subheader('My sub')
+st.code('for i in range(8): foo()')
 
-# Show a spinner during a process
-with st.spinner(text='In progress'):
-  time.sleep(3)
-  st.success('Done')
-
-# Show and update progress bar
-bar = st.progress(50)
-time.sleep(3)
-bar.progress(100st.balloonsst.snow()
-st.toast('Mr Stay-Puft')
-sterror('Error message')
-st.arning('Warning message')
-st.info('Info message')
-st.success('Success message')
-st.exception(e)
+# * optional kwarg unsafe_allow_html = True
